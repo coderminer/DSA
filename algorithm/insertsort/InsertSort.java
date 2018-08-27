f50c06e2eced78aa56961cba8e62718d2e2fbea9
kevin <kevin@puhanda.com>
2018-08-27 18:49:04 +0800

    add insert sort algorithm

---
+++ algorithm/insertsort/InsertSort.java
@@ -0,0 +1,15 @@
+/**
+ * InsertSort
+ */
+public class InsertSort {
+
+    private static void swap(Comparable[] a,int i,int j){
+        Comparable tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    public void insertSort(Comparable[] arr){
+        
+    }
+}
